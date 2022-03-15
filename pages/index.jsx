eaacf831f74edc0d99b838c1c56e692b7f38d6b9
Fuadzarzar <fuadzarzar@outlook.com>
2022-03-15 16:44:46 -0300

    introducing of animation

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,15 +2,24 @@ import Header from '../components/Header/Header'
 import LatestProject from '../components/LatestProject/Latest'
 import styles from '../components/Header/header.module.css'
 import Portfolio from '../components/Portfolio/Portfolio'
+import Head from 'next/head'
 
 export default function Start() {
 
     return (
         <>
+        <div>
+            <Head>
+                <title>Fuad Zarzar - Portfolio</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+        </div>
         <body className={styles.body}>
-        <Header />
-        <LatestProject />
-        < Portfolio />
+                <Header />
+            <div className={styles.shortcuts}>
+                <LatestProject />
+                < Portfolio />
+            </div>
         </body>
         </>
 
